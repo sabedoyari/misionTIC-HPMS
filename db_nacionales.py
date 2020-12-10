c3aac32b680eb4c679f1c82cfaf17503d310e618
Riguja89 <jaimegutierrios@gmail.com>
2020-12-10 00:12:05 -0500

    solo nacionales

--- db_nacionales.py
+++ db_nacionales.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 
-class Temporada(BaseModel):
+class Fiesta(BaseModel):
     fecha_inicio: str
     fecha_fin: str
     ciudad: str
@@ -12,18 +12,19 @@ class Temporada(BaseModel):
 Festividades_alta= {
     1: Fiesta ( fecha_inicio="02-01-2021", fecha_fin="07-01-2021", ciudad="Pasto", fiesta="Carnaval de Negros y Blancos"),
     2: Fiesta ( fecha_inicio="04-01-2021", fecha_fin="12-01-2021", ciudad="Cartagena", fiesta="Festival Internacional de Música"),
-    3: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
-    3: Fiesta ( fecha_inicio="29-04-2021", fecha_fin="02-05-2020", ciudad="Valledupar", fiesta="Festival de la Leyenda Vallenata"),
-    3: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
-    3: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
-    3: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
-    3: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
+    4: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
+    5: Fiesta ( fecha_inicio="29-04-2021", fecha_fin="02-05-2020", ciudad="Valledupar", fiesta="Festival de la Leyenda Vallenata"),
+    6: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
+    7: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
+    8: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
+    9: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
 
 }
 
 
 def obtener_temporada():
     # Haga lo que tenga que hacer para conectarse a la base de datos y obtener todas las ordenes
-    lista_ordenes = []
-    for e in ordenes:
-        list
+    lista_temporadas = []
+    for e in Festividades_alta:
+        lista_temporadas.append(Festividades_alta)
+        return lista_temporadas
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-import db_general
+#import db_general
 import db_nacionales
 
 
@@ -7,16 +7,16 @@ app = FastAPI()
 
 
 @app.get("/Temporada/")
-async def obtener_Temporada():
-    Temporada = db.obtener_Temporada()
+async def obtener_temporada():
+    Temporada = db_nacionales.obtener_temporada()
     return  Temporada
 
 
-@app.post("/ordenes/crear/")
-async def crear_orden(orden: db.Orden):
-    creada_exitosamente = db.crear_orden(orden)
-    if creada_exitosamente:
-        return {"mensaje": "Orden creada correctamenteo"}
-    else:
-        raise HTTPException(
-            status_code=400, detail="error, orden con ese id ya exisitia")
+#@app.post("/ordenes/crear/")
+#async def crear_orden(orden: db.Orden):
+ #   creada_exitosamente = db.crear_orden(orden)
+  #  if creada_exitosamente:
+   #     return {"mensaje": "Orden creada correctamenteo"}
+    #else:
+     #   raise HTTPException(
+      #      status_code=400, detail="error, orden con ese id ya exisitia")
