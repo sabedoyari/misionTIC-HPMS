e6870f67c83fa0682975f59400ebe18e0a7fbe95
santiago.bedoya.rios@gmail.com <santiago.bedoya.rios@gmail.com>
2020-12-09 22:03:09 -0500

    Initial commit to upload the first files

---
+++ db_nacionales.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+
+class Temporada(BaseModel):
+    fecha_inicio: str
+    fecha_fin: str
+    ciudad: str
+    fiesta: str
+
+
+
+Festividades_alta= {
+    1: Fiesta ( fecha_inicio="02-01-2021", fecha_fin="07-01-2021", ciudad="Pasto", fiesta="Carnaval de Negros y Blancos"),
+    2: Fiesta ( fecha_inicio="04-01-2021", fecha_fin="12-01-2021", ciudad="Cartagena", fiesta="Festival Internacional de Música"),
+    3: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
+    3: Fiesta ( fecha_inicio="29-04-2021", fecha_fin="02-05-2020", ciudad="Valledupar", fiesta="Festival de la Leyenda Vallenata"),
+    3: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
+    3: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
+    3: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
+    3: Fiesta ( fecha_inicio="21-04-2021", fecha_fin="05-05-2021", ciudad="Bogota", fiesta="Feria del Libro"),
+
+}
+
+
+def obtener_temporada():
+    # Haga lo que tenga que hacer para conectarse a la base de datos y obtener todas las ordenes
+    lista_ordenes = []
+    for e in ordenes:
+        list
---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, HTTPException
+import db
+
+app = FastAPI()
+
+
+@app.get("/Temporada/")
+async def obtener_Temporada():
+    Temporada = db.obtener_Temporada()
+    return  Temporada
+
+
+@app.post("/ordenes/crear/")
+async def crear_orden(orden: db.Orden):
+    creada_exitosamente = db.crear_orden(orden)
+    if creada_exitosamente:
+        return {"mensaje": "Orden creada correctamenteo"}
+    else:
+        raise HTTPException(
+            status_code=400, detail="error, orden con ese id ya exisitia")
